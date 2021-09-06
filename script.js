95df666f83bad31b00ed937b2f77464450d128ab
Mya <myaskylar14@gmail.com>
2021-09-06 19:16:27 +0100

    Level 100 complete

--- script.js
+++ script.js
@@ -2,11 +2,46 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `You Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `You Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach((episode) => {
+    //Create card for each episode details
+    const divCard = document.createElement("div");
+    divCard.setAttribute("class", "card");
+
+    //Create anchors tag to link to original site for particular episode.
+    const linkToSite = document.createElement("a");
+    linkToSite.href = episode.url;
+    linkToSite.style.textDecoration = "none";
+    linkToSite.style.color = "black";
+
+    //image of episode (medium size)
+    const showCover = document.createElement("img");
+    showCover.src = episode.image.medium;
+
+    const showTitle = document.createElement("h1");
+    showTitle.innerText = episode.name;
+
+    // episode season and number combine in (S01E01) method
+    const showEpisode = document.createElement("h2");
+    let showSeason = episode.season.toString().padStart(2, 0);
+    let episodeNumber = episode.number.toString().padStart(2, 0);
+    showEpisode.innerText = `S${showSeason}E${episodeNumber}`;
+
+    const epiSummary = document.createElement("p");
+    epiSummary.innerHTML = episode.summary;
+
+    //click on card will direct to the specific episode on that site
+    linkToSite.append(showCover, showTitle, showEpisode, epiSummary);
+    divCard.appendChild(linkToSite);
+    rootElem.appendChild(divCard);
+  });
+
 }
 
 window.onload = setup;
